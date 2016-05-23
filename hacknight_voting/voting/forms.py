ffefb8c8f557fccd24a10fa451cd5f5773184109
ndevox <nick.a.sarbicki@gmail.com>
2016-05-23 20:15:32 +0100

    Adding forms for votes

--- hacknight_voting/voting/forms.py
+++ hacknight_voting/voting/forms.py
@@ -1,6 +1,8 @@
-from django import forms
+from django import forms, Valida
+from django.forms import ValidationError
 
 from voting.models import HackNight
+
 
 class optionForm(forms.Form):
     option = forms.CharField(label='option', max_length=255)
@@ -15,6 +17,24 @@ class VoteForm(forms.Form):
 class FirstVoteForm(VoteForm):
     options = forms.MultipleChoiceField()
 
+    def clean(self, *args, **kwargs):
+        cleaned_data = super(FirstVoteForm, self).clean(*args, **kwargs)
+        if len(self.options) < 1:
+            raise ValidationError('You need to pick an option')
+
+        cleaned_data['hacknight'] = self.hacknight
+
+        return cleaned_data
+
 
 class SecondVoteForm(VoteForm):
     option = forms.ChoiceField()
+
+    def clean(self, *args, **kwargs):
+        cleaned_data = super(FirstVoteForm, self).clean(*args, **kwargs)
+        if len(self.options) != 1:
+            raise ValidationError('You must pick one option')
+
+        cleaned_data['hacknight'] = self.hacknight
+
+        return cleaned_data
