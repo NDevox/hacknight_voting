648b761a6c974e824d024f70b03421e61ab5dd91
ndevox <nick.a.sarbicki@gmail.com>
2016-05-23 20:21:38 +0100

    Fixing view from url

--- hacknight_voting/voting/urls.py
+++ hacknight_voting/voting/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from voting import views
 
 urlpatterns = [
-    url(r'^option/$', views.Options, name='add_option'),
-    url(r'^vote1/$', views.FirstVote, name='first_vote'),
-    url(r'^vote2/$', views.SecondVote, name='second_vote'),
+    url(r'^option/$', views.registerOption.as_view(), name='add_option'),
+    url(r'^vote1/$', views.FirstVote.as_view(), name='first_vote'),
+    url(r'^vote2/$', views.SecondVote.as_view(), name='second_vote'),
 ]
