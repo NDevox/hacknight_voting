da9a5fa1ef0cd925a692669d1ccf325a407eedd2
ndevox <nick.a.sarbicki@gmail.com>
2016-05-23 19:55:34 +0100

    Adding first vote.

--- hacknight_voting/voting/views.py
+++ hacknight_voting/voting/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
-# Create your views here.
+
+class FirstVote(View):
+
+    def get(self, request, *args, **kwargs):
+        options = self.hacknight.options.all()
+        context = {'options': options}
+        return render(request, 'vote1.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form  = self.FORM
+
+        if form.is_valid():
+            form.run_votes()
+
+        return redirect('voting:second_vote')
